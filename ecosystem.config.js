a334a2889fadfba5f12aba5d38b4951b23525e3a
Jared <j.rogers1@live.com>
2025-04-29 09:55:39 -0700

    revert: Remove empty post-deploy script attempt

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -19,7 +19,7 @@ module.exports = {
       // ref: 'origin/master', // Git branch
       // repo: 'GIT_REPOSITORY', // Your Git repo
       path: '/home/deploy/current', // Deployment path on server - **Important**
-      'post-deploy': '', // Attempt to override default script
+      // 'post-deploy': '', // Attempt to override default script
       // Optional: specify environment variables for the deployed app
       // 'env': {
       //   'NODE_ENV': 'production'
